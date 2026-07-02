9be4f57c5c3a5d2effbda9af65f38f8eb3cca9c2
Clev123 <clevenchin08@gmail.com>
2026-07-02 15:45:38 +0800

    lab3

---
+++ labs/lab03/exercise5/lab-03-5.py
@@ -0,0 +1,3 @@
+packsEaten = int(input())
+totalCalories = packsEaten * 300 * 10
+print(totalCalories)
---
+++ labs/lab03/exercise6/lab-03-6.py
@@ -0,0 +1,6 @@
+yardLength = float(input())
+yardWidth = float(input())
+houseLength = float(input())
+houseWidth = float(input())
+wage = houseLength * houseWidth - yardLength * yardWidth * 2
+print(wage)
